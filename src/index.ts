f7d2b0338a5aea20a7942055c0dba2b8a1ddd8af
Simon Lagos <simon.lagos.sallhed@gmail.com>
2021-12-19 16:30:57 +0100

    Add the first version of the library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,50 @@
-export const sayHello = () => {
-  return `Hello world!`
+// Simple 2d vector functions. It's not an exhaustive list.
+
+export type Vector = {
+  x: number
+  y: number
 }
+
+export const create = (x: number, y: number): Vector => {
+  return { x, y }
+}
+
+export const add = (vector: Vector, value: number): Vector => ({
+  x: vector.x + value,
+  y: vector.y + value,
+})
+
+export const subtract = (vector: Vector, value: number): Vector => ({
+  x: vector.x - value,
+  y: vector.y - value,
+})
+
+export const divide = (vector: Vector, value: number): Vector => ({
+  x: vector.x / value,
+  y: vector.y / value,
+})
+
+export const multiply = (vector: Vector, value: number): Vector => ({
+  x: vector.x * value,
+  y: vector.y * value,
+})
+
+export const floor = (vector: Vector): Vector => ({
+  x: Math.floor(vector.x),
+  y: Math.floor(vector.y),
+})
+
+export const ceil = (vector: Vector): Vector => ({
+  x: Math.ceil(vector.x),
+  y: Math.ceil(vector.y),
+})
+
+export const normalize = ({ x, y }: Vector): Vector => {
+  const magnitude = Math.sqrt(x ** 2 + y ** 2)
+  return {
+    x: x / magnitude ?? 0,
+    y: y / magnitude ?? 0,
+  }
+}
+
+export const clone = (vector: Vector): Vector => ({ x: vector.x, y: vector.y })
--- test.ts
+++ test.ts
@@ -1,7 +1,33 @@
 import test from 'ava'
 
-import { sayHello } from 'vcr'
+import * as vcr from 'vcr'
 
-test('sayHello', (t) => {
-  t.is(sayHello(), 'Hello world!')
+test('create', (t) => {
+  t.deepEqual(vcr.create(10, 10), { x: 10, y: 10 })
 })
+
+test('add', (t) => {
+  t.deepEqual(vcr.add({ x: 10, y: 10 }, 5), { x: 15, y: 15 })
+})
+
+test('subtract', (t) => {
+  t.deepEqual(vcr.subtract({ x: 10, y: 10 }, 5), { x: 5, y: 5 })
+})
+
+test('divide', (t) => {
+  t.deepEqual(vcr.divide({ x: 15, y: 15 }, 5), { x: 3, y: 3 })
+})
+
+test('multiply', (t) => {
+  t.deepEqual(vcr.multiply({ x: 10, y: 10 }, 5), { x: 50, y: 50 })
+})
+
+test('floor', (t) => {
+  t.deepEqual(vcr.floor({ x: 10.456, y: 10.789 }), { x: 10, y: 10 })
+})
+
+test('ceil', (t) => {
+  t.deepEqual(vcr.ceil({ x: 10.456, y: 10.789 }), { x: 11, y: 11 })
+})
+
+test.todo('normalize')
